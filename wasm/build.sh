55e29775862fba90c95fb9c70882bac614c7371d
Wolf McNally <wolf@wolfmcnally.com>
2021-10-28 20:09:04 +0200

    Working on support for WebAssembly.

--- wasm/build.sh
+++ wasm/build.sh
@@ -2,19 +2,17 @@
 
 set +e
 
-emcc -Oz -o lifehash.js ../src/*.cpp
-
 mkdir -p lib
 
 emcc \
-    ../src/*.cpp \
-    -Oz \
     -o ./lib/lifehash.js \
+    -Oz \
     -s EXPORTED_FUNCTIONS='[_free,_malloc]' \
     -s EXPORT_ES6=1 \
     -s MODULARIZE=1 \
     -s ENVIRONMENT='web' \
     -s NO_DISABLE_EXCEPTION_CATCHING \
-    --post-js lifehash.post.js
+    --post-js lifehash.post.js \
+    ../src/*.cpp
 
 cp lifehash.d.ts ./lib/
